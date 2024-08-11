372214180c6bb03263a5bf9916f339ec41e7d5ee
hamza19901990 <74471418+hamza19901990@users.noreply.github.com>
2024-08-11 19:35:22 +0300

    Update hhv.py

--- hhv.py
+++ hhv.py
@@ -1,36 +1,40 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import streamlit as st
 from PIL import Image
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 import pickle
 
+# App title and introduction
 st.write("""
 # HHV Prediction of MSW
 This app predicts the **Higher Heating Value (HHV) of Municipal Solid Waste (MSW)**!
 """)
 st.write('---')
 
-image = Image.open(r'soil1.jpg')
+# Display an image
+image = Image.open('soil1.jpg')
 st.image(image, use_column_width=True)
 
-data = pd.read_csv(r"msw_all.csv")
+# Load dataset and display information
+data = pd.read_csv('msw_all.csv')
 req_col_names = ["C_Percentage", "H_Percentage", "N_Percentage", "S_Percentage", "O_Percentage", "HHV"]
 curr_col_names = list(data.columns)
 
-mapper = {}
-for i, name in enumerate(curr_col_names):
-    mapper[name] = req_col_names[i]
-
+mapper = {curr_col_names[i]: req_col_names[i] for i in range(len(curr_col_names))}
 data = data.rename(columns=mapper)
+
 st.subheader('Data Information')
 st.write(data.head())
 st.write(data.isna().sum())
+
+# Display correlation matrix
 corr = data.corr()
 st.write(corr)
 
-
+# Sidebar for user input parameters
 st.sidebar.header('Specify Input Parameters')
 def get_input_features():
     C_Percentage = st.sidebar.slider('C_Percentage', 9.00, 92.00, 10.00)
@@ -38,7 +42,6 @@ def get_input_features():
     N_Percentage = st.sidebar.slider('N_Percentage', 0.00, 10.00, 5.00)
     S_Percentage = st.sidebar.slider('S_Percentage', 0.00, 2.64, 2.00)
     O_Percentage = st.sidebar.slider('O_Percentage', 0.00, 48.62, 44.72)
-   
     
     data_user = {
         'C_Percentage': C_Percentage,
@@ -53,16 +56,20 @@ def get_input_features():
 
 df = get_input_features()
 
-# Main Panel
+# Main panel to display specified input parameters
 st.header('Specified Input Parameters')
 st.write(df)
 st.write('---')
 
-# Reads in saved classification model
-load_clf = pickle.load(open('new (2).pkl', 'rb'))
+# Load the scaler and the trained model
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+model = pickle.load(open('new (2).pkl', 'rb'))
 
+# Standardize the input data using the loaded scaler
+df_std = scaler.transform(df)
+
+# Prediction
 st.header('Prediction of HHV (MJ/kg)')
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-st.write(prediction)
-st.write('---') 
+prediction = model.predict(df_std)
+st.write(prediction[0])
+st.write('---')
